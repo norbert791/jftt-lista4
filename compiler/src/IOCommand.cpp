ac8267f1ce84399adb514c276773f976d00e681e
Norbert Jaśniewicz <norbert.jasniewicz@protonmail.com>
2023-01-07 11:12:45 +0100

    Zaimplementowano IO w funkcjach.

--- compiler/src/IOCommand.cpp
+++ compiler/src/IOCommand.cpp
@@ -4,10 +4,25 @@
 namespace compilerLogic{
   std::vector<IntermidiateCode> IOCommand::parseIntermidiate() {
     std::vector<IntermidiateCode> result;
+    /*
+      Perform different IO operation based of IO type.
+      Also if variable is a reference, then it has to be uploaded to
+      acc (mem 0) before printing.
+    */
     if (this->type == EIOType::INPUT) {
-      result.push_back({EInstruction::GET, EParameterType::VAR_ID, this->var->getId()});
+      if (this->var->getType() == EVariableType::VALUE) {
+        result.push_back({EInstruction::GET, EParameterType::VAR_ID, this->var->getId()});
+      } else {
+        result.push_back({EInstruction::GET, EParameterType::VALUE, 0});
+        result.push_back({EInstruction::STOREI, EParameterType::VAR_ID, this->var->getId()});
+      }
     } else {
-      result.push_back({EInstruction::PUT, EParameterType::VAR_ID, this->var->getId()});
+      if (this->var->getType() == EVariableType::VALUE) {
+        result.push_back({EInstruction::PUT, EParameterType::VAR_ID, this->var->getId()});
+      } else {
+        result.push_back({EInstruction::LOADI, EParameterType::VAR_ID, this->var->getId()});
+        result.push_back({EInstruction::PUT, EParameterType::VALUE, 0});
+      }
     }
 
     return result;
